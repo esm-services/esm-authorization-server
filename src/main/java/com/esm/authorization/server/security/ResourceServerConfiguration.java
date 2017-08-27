96f7cc563d3867a4401399827a421c43d9b42090
Durgesh RAI <drai548@gmail.com>
2017-08-27 17:45:22 +0530

    updated configuration

--- src/main/java/com/esm/authorization/server/security/ResourceServerConfiguration.java
+++ src/main/java/com/esm/authorization/server/security/ResourceServerConfiguration.java
@@ -5,16 +5,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import lombok.AllArgsConstructor;
 
 @Configuration
+@AllArgsConstructor
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 	
 	 public static final String RESOURCE_ID = "rest_api";
+	 
+	 private final TokenStore tokenStore;
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-		 resources.resourceId(RESOURCE_ID);
+		 resources.resourceId(RESOURCE_ID).tokenStore(tokenStore);
 	}
 
 	@Override
